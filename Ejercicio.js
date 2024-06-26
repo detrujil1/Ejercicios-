ca5ca0f98b2a187acab70d52728540e676cbf902
detrujil1 <ingtrujillo6@gmail.com>
2024-06-26 15:23:39 -0500

    Commit inicial

---
+++ Ejercicio.js
@@ -0,0 +1,8 @@
+let Bienvenida = prompt("¡Bienvenida y bienvenido a nuestro sitio web!");
+let nombre = prompt("¡Bienvenida y bienvenido a nuestro sitio web!");
+let edad = 25;
+let numeroDeVentas = 50;
+let saldoDisponible = 1000;
+mensajeDeError =  alert("¡Error! Completa todos los campos");
+
+
---
+++ practica.js
@@ -0,0 +1,15 @@
+console.log ("bienvenida");
+let nombre = "Daniel"  
+console.log(`Hola  ${nombre}`);
+let valor1 =4;
+let valor2 =6;
+resultado = valor1 + valor2
+console.log (`La suma de ${valor1} y ${valor2} es igual a ${resultado}`)
+
+let nota =Math.floor(Math.random()*1000)+1;
+console.log(nota)
+if (nota >=7){
+   console.log("Aprobado")
+} else{
+   console.log("Reprobado")
+}
